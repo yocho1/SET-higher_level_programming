079ce51870fe0c5e6ab5a3ea2d16a2b6446211dc
yocho1 <achraflachgar456@@gmail.com>
2026-09-15 15:02:44 -0700

    Fix 6-square.js - rename import to avoid naming conflict

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const Square = require('./5-square');
+const SquareParent = require('./5-square');
 
-class Square extends Square {
+class Square extends SquareParent {
   charPrint (c = 'X') {
     for (let i = 0; i < this.height; i++) {
       console.log(c.repeat(this.width));

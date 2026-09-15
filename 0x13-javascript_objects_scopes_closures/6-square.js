bcf81c33d62bc99bcd0a6e5d9221151cd5412301
yocho1 <achraflachgar456@@gmail.com>
2026-09-15 15:00:55 -0700

    Add 6-square.js - Square with charPrint method

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const Square = require('./5-square');
+
+class Square extends Square {
+  charPrint (c = 'X') {
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
